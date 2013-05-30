a301ab453692023d4375285179c67429b56c1300
Zidni Mubarock <zidmubarock@gmail.com>
2013-05-31 02:26:40 +0700

    holiday started

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'sqlite3'
+
+gem 'jquery-rails'
+gem 'haml-rails'
+gem 'devise'
+gem 'cancan'
+gem 'strong_parameters'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'rspec-rails'
+
+group :test do
+	gem 'rr'
+	gem 'database_cleaner', :git => 'https://github.com/bmabey/database_cleaner.git'
+	gem 'factory_girl_rails'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpkKredit::Application.config.secret_token = '380238d234d0888da7b7138ebb03d5892467d2754f45b9b57a89fa38f9eafc85b1480c60f72cde30c3212106f7c9be99ef1b459a710611ec26a42c9ac8ec44c3'
